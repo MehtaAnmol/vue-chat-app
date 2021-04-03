d8fbe9c31903db7ae8726cc4ec6e4b2b433e6b2f
Anmol Mehta <mehta.anmol48@gmail.com>
2021-04-03 17:06:59 +0530

    Added chatroom functionality

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,89 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld/>
-    </v-main>
+    <div class="header">
+      <h1>Chatroom</h1>
+      <p class="username">Username:{{ username }}</p>
+      <p class="online">Online: {{ users.length }}</p>
+    </div>
+    <chat-room :messages="messages" @sendMessage="sendMessage" />
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
+import io from "socket.io-client";
+import Chatroom from "./components/Chatroom";
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
-    HelloWorld,
+    "chat-room": Chatroom,
   },
 
   data: () => ({
-    //
+    username: "",
+    socket: io("http://localhost:3000"),
+    messages: [],
+    users: [],
   }),
+
+  methods: {
+    joinServer() {
+      this.socket.on("loggedIn", (data) => {
+        this.messages = data.messages;
+        this.users = data.users;
+        this.socket.emit("newUser", this.username);
+      });
+
+      this.listen();
+    },
+
+    listen() {
+      this.socket.on("msg", (message) => {
+        this.messages.push(message);
+      });
+
+      this.socket.on("userOnline", (user) => {
+        this.users.push(user);
+      });
+
+      this.socket.on("userLeft", (user) => {
+        this.users.splice(this.users.indexOf(user), 1);
+      });
+    },
+
+    sendMessage(message) {
+      this.socket.emit("msg", message);
+    },
+  },
+
+  mounted() {
+    this.username = prompt("What is your username", "Anonymous");
+
+    if (!this.username) {
+      this.username = "Anonymous";
+    }
+
+    this.joinServer();
+  },
 };
 </script>
+
+<style lang="scss">
+body {
+  font-family: "Avenir", Arial, Helvetica, sans-serif;
+  color: #2c3e50;
+  margin: 0;
+  padding: 0;
+}
+
+#app {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  width: 100%;
+  max-width: 768px;
+  margin: 0 auto;
+  padding: 15px;
+  box-sizing: border-box;
+}
+</style>
